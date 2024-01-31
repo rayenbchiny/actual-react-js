8cf8ab8e4eaae6ff24db5adf87a36fd5bbac7ee6
rayenbchiny <rayen.bchini21@gmail.com>
2024-01-31 14:29:57 +0100

    first commt

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Mohamed from "./Card";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css"
+import Crd from "./Crd";
+
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App"> 
+     <Mohamed/>
+     <header className="Crd">
+    <Crd/>
+    <Crd/>
+    <Crd/>
+    </header>
+    </div>
+    
+    
+    
+    
+    
+    
+    </>
+
+
+
+
+
+  );
+}
+
+export default App;
